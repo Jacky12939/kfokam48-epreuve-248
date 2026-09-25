94a5ae2a35b24f435fd2b3e1257aafb7bfafac43
Jacky12939 <jacquelinetagne6@gmail.com>
2026-09-25 14:01:22 +0100

    chore(backend): init Spring Boot + wrapper mvnw (B1)

---
+++ backend/src/main/java/cm/kfokam48/presence/PresenceAppApplication.java
@@ -0,0 +1,13 @@
+package cm.kfokam48.presence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceAppApplication.class, args);
+	}
+
+}
